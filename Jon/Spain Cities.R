c80a9166e9a071aad2b817dcd5073983ecefd2b0
jonacha <jonacha@opendeusto.es>
2018-01-08 11:11:02 +0100

    Arreglado todas las distancias y los tiempo
    Falta eliminar la columna 1 y la fila 1

--- Jon/Spain Cities.R
+++ Jon/Spain Cities.R
@@ -41,9 +41,13 @@ colnames(fallos) <- cnames
 
 nfallos=1;
 for(i in 1:ContCiudades){
-  for(j in 1:ContCiudades){
+  for(j in i:ContCiudades){
+    if(j==1){
+      j=i;
+    }
+    else{
     if(i!=j){
-    datos = mapdist(Ciudades$Ciudades[i], Ciudades$Ciudades[j], mode="driving")
+      datos = mapdist(Ciudades$Ciudades[i], Ciudades$Ciudades[j], mode="driving")
     a=datos[,3]
     if( is.na(a)){
     fallos[nfallos,1]=datos[,1]
@@ -56,10 +60,32 @@ for(i in 1:ContCiudades){
       tiempo[i,j]= datos[,7]
     }
     }
+    }
     
   }
 }
+write.xlsx(distancias, "C:/Proyecto/RStrudioProyect/Jon/DistanciasCuidades.xlsx") 
+write.xlsx(tiempo, "C:/Proyecto/RStrudioProyect/Jon/TiemposCiudades.xlsx") 
 
+a=distancias
+b=tiempo;
+class(a)
+for(i in 1:ContCiudades){
+  a[1:(52-i),i]=distancias[i,1:(52-i)]
+  b[1:(52-i),i]=tiempo[i,1:(52-i)]
+  
+}
+
+for(i in 1:ContCiudades){
+  a[i,1:i]=distancias[1:i,i]
+  b[i,1:i]=tiempo[1:i,i]
+  
+}
+
+
+a[1:52,1]
+a[1:52,1]=distancias[1,1:52]
+distancias[1,1:52]
 #Santa+Cruz+de+Tenerife&mode=driving&sensor=false
 #matching was not perfect, returning what was found.
 #Error in `*tmp*`[[c(1, 1)]] : no such index at level 1
@@ -67,7 +93,6 @@ for(i in 1:ContCiudades){
 #coord = geocode(puntos$localizacion)
 #puntos$lat = coord$lat
 #puntos$lon = coord$lon
-
 
 
 #Zona de pruevas----------------------------------------------------------------------------------
@@ -78,22 +103,28 @@ if(is.data.frame(datos) && nrow(datos)==0){
   datos[,7]=0;
 }
 
-puntos = data.frame(localizacion = c("Vitoria",
-                                     "barcelona",
-                                     "Roma"), stringsAsFactors = F)
+puntos = data.frame(localizacion = c("Barcelona",
+                                     "bilbao",
+                                     "Albacete","Centro Comercial Ferial Plaza, Guadalajara","Catedral de Leon","Tenerife","Parque De Santa Ana, Cuenca","Museo Maritimo del Cantabrico,Santander"), stringsAsFactors = F)
 
-distancias = mapdist(puntos$localizacion[1], puntos$localizacion[2], mode="driving")
-
-
-distancias = mapdist(puntos$localizacion[2], puntos$localizacion[3], mode="driving")
-
+ad = mapdist("Barcelona", "bilbao", mode="driving", output = c( "all"), messaging = FALSE, sensor = FALSE, language = "en-EN", override_limit = FALSE)
+a = mapdist(puntos$localizacion[3], puntos$localizacion[2], mode="driving")
+#a = mapdist(puntos$localizacion[4],puntos$localizacion[2],mode="driving")
+#a = mapdist(puntos$localizacion[5],puntos$localizacion[2],mode="driving")
+a = mapdist(puntos$localizacion[6], puntos$localizacion[2], mode="driving")
+a = mapdist(puntos$localizacion[7], puntos$localizacion[2], mode="driving")
+a = mapdist(puntos$localizacion[8], puntos$localizacion[2], mode="driving")
 geocodeQueryCheck(userType = "free")
 cordenadas
 cordenadas<-  geocode("Melilla")
 cordenadas=0;
 
+puntos = data.frame(localizacion = c("Plaza Nueva Bilbao",
+                                     "Paris",
+                                     "Roma"), stringsAsFactors = F)
 
-
+d = mapdist(puntos$localizacion[2], puntos$localizacion[3], mode="driving")
+rute = route(puntos$localizacion[2], puntos$localizacion[3], structure="route")
 #Antiguo
 
 leaflet(Ciudades) %>% addTiles() %>%
